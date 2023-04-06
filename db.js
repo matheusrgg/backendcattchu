57f4b7cd49375b02a729489ebb5ca30ec4efbfff
matheusrgg <matheusrgg@gmail.com>
2023-04-06 11:09:07 -0300

    subindo Heroku

--- db.js
+++ db.js
@@ -3,13 +3,12 @@ require('dotenv').config()
 const sequelize = new Sequelize('bancoclina','postgres','123',{
     dialect:'postgres',
     host:'localhost',
-    port:5432
-    ,
+    port:5432    ,
     ssl: true
 })
 
 
-
+// -------------->Producao
 // const sequelize = new Sequelize(process.env.DATABASE_URL,{
 //     dialectOptions: {
 //         ssl: {
