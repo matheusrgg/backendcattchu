aba9651bfe0a42df5942ba09bcaee98887e042b0
Mauricio Edurdo da Rocha Junior <mauricio.junior.edu@gmail.com>
2023-04-12 13:37:19 -0300

    adicionando routes para empresa

--- routes/empresa.routes.js
+++ routes/empresa.routes.js
@@ -6,5 +6,8 @@ const EmpresaController = require('../controller/empresa.controller')
 
 
 router.post("/create", EmpresaController.createEmpresa)
+router.get("/list", EmpresaController.listEmpresa)
+router.get("/list/:id", EmpresaController.idEmpresa)
+router.post("/login", EmpresaController.loginEmpresa)
 
 module.exports = router
