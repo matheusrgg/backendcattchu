e31bca80f3184a8c607cb581d1dc806da020b1be
matheusrgg <matheusrgg@gmail.com>
2023-05-15 22:24:40 -0300

    funcionalidade proposal edit

--- controller/proposta.controller.js
+++ controller/proposta.controller.js
@@ -52,12 +52,53 @@ class PropostaController {
     const propostas = await Proposta.findAll({
     where:{id_remetente: idUser}})
     return res.status(201).send(propostas);
-
-
-    // const influenciador = await Proposta.findByPk(req.params.id)
-    // return res.status(201).send(influenciador);
   }
 
-}
+  static async listIdProposta(req, res){
+    const proposta = await Proposta.findByPk(req.params.id)
+    return res.status(201).send(proposta);
+  }
 
+  static async updateProposal(req, res){
+    try {
+    var proposta = await Proposta.findByPk(req.params.id);
+    const{
+      mensagem_proposta,
+      veiculo_midiatico,
+      valor_divulgacao,
+      status_proposta,
+      tipo_remetente,
+      id_remetente,
+      id_destinatario,
+      influenciadorId,
+      empresaId,
+      data_envio
+    } = req.body;
+
+    const data = {
+      mensagem_proposta: mensagem_proposta,
+      veiculo_midiatico :veiculo_midiatico,
+      valor_divulgacao :valor_divulgacao,
+      status_proposta:status_proposta,
+      tipo_remetente :tipo_remetente,
+      id_remetente:id_remetente,
+      id_destinatario:id_destinatario,
+      influenciadorId:influenciadorId,
+      empresaId:empresaId,
+      data_envio :data_envio
+    }
+
+    const where = {
+      where: {
+        id: req.params.id
+      }
+    }
+    proposta = await Proposta.update(data, where);
+    return res.status(201).send(data);
+  }
+   catch (error) {
+      console.log(error);
+    }
+}
+}
 module.exports = PropostaController
--- routes/proposta.routes.js
+++ routes/proposta.routes.js
@@ -8,4 +8,8 @@ const PropostaController = require("../controller/proposta.controller")
 router.post("/create", PropostaController.createProposta)
 router.get("/list", PropostaController.listAllProposta)
 router.get("/list/:id", PropostaController.listPropostaFromUser)
+router.get("/listOne/:id", PropostaController.listIdProposta)
+router.put("/update/:id", PropostaController.updateProposal)
+
+
 module.exports = router
