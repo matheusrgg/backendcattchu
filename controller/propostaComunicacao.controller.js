6d695827d1030df6af28c935d6333b97b09308d9
matheusrgg <matheusrgg@gmail.com>
2023-06-29 14:45:09 -0300

    logica proposta

--- controller/propostaComunicacao.controller.js
+++ controller/propostaComunicacao.controller.js
@@ -7,99 +7,140 @@ class PropostaComunicacaoController {
   constructor() { }
 
 
-//---->Influenciador Envio e Recebimento
+  //---->Influenciador Envio e Recebimento
 
-static async listPropostaFromInfluencerEnviadas(req, res){
-  const idUser = req.params.id
-  const propostas = await Proposta.findAll({
-  where:{
-    id_remetente: idUser,
-    tipo_remetente:'influenciador'
-  },
-//   include: {
-//     model: Influenciador,
-//     required: true
-//   },
-  include: {
-    model: Empresa,
-    required: true
+  static async listPropostaFromInfluencerEnviadas(req, res) {
+    const idUser = req.params.id
+    const propostas = await Proposta.findAll({
+      where: {
+        id_remetente: idUser,
+        tipo_remetente: 'influenciador'
+      },
+
+      include: {
+        model: Empresa,
+        required: true
+      }
+
+
+    })
+    const propstaComNome = propostas.map(data => {
+      return {
+        id: data.id,
+        status_proposta: data.status_proposta,
+        mensagem_proposta: data.mensagem_proposta,
+        tipo_remetente: data.tipo_remetente,
+        updated: data.updated,
+        id_remetente: data.id_remetente,
+        id_destinatario: data.id_destinatario,
+        id_empresa: data.id_empresa,
+        data_envio: data.createdAt,
+        nome: data.Empresa.nome
+      }
+    })
+    return res.status(201).send(propstaComNome);
   }
-  
-  
-})
-const propstaComNome = propostas.map(data =>{
-  return{
-    id:data.id,
-    status_proposta:data.status_proposta ,
-    mensagem_proposta:data.mensagem_proposta ,
-		tipo_remetente:data.tipo_remetente ,
-		updated:data.updated ,
-		id_remetente:data.id_remetente ,
-		id_destinatario:data.id_destinatario ,
-		id_influenciador: data.id_influenciador,
-        data_envio:data.createdAt,
-    // nome_influenciador:data.Influenciador.nome,
-    nome_empresa : data.Empresa.nome
+
+
+  static async listPropostaFromInfluencerRecebidas(req, res) {
+    const idUser = req.params.id
+    const propostas = await Proposta.findAll({
+      where: {
+        id_influenciador: idUser,
+        tipo_remetente: 'marca',
+      },
+      include: {
+        model: Empresa,
+        required: true
+      }
+
+
+    })
+    const propstaComNome = propostas.map(data => {
+      return {
+        id: data.id,
+        status_proposta: data.status_proposta,
+        mensagem_proposta: data.mensagem_proposta,
+        tipo_remetente: data.tipo_remetente,
+        updated: data.updated,
+        id_remetente: data.id_remetente,
+        id_destinatario: data.id_destinatario,
+        id_empresa: data.id_empresa,
+        data_envio: data.createdAt,
+        nome: data.Empresa.nome
+      }
+    })
+    return res.status(201).send(propstaComNome);
   }
-})
-  return res.status(201).send(propstaComNome);
-}
 
 
-static async listPropostaFromInfluencerRecebidas(req, res){
-  const idUser = req.params.id
-  const propostas = await Proposta.findAll({
-  where:{
-    id_influenciador :idUser,
-    tipo_remetente:'marca',
-  }, include: {
-    model: Influenciador,
-    required: true
+  //---->Empresa Envio e Recebimento
+
+
+  static async listPropostaFromEmpresaEnviadas(req, res) {
+    const idUser = req.params.id
+    const propostas = await Proposta.findAll({
+      where: {
+        id_remetente: idUser,
+        tipo_remetente: 'marca'
+      },
+      include: {
+        model: Influenciador,
+        required: true
+      },
+
+
+
+    })
+    const propstaComNome = propostas.map(data => {
+      return {
+        id: data.id,
+        status_proposta: data.status_proposta,
+        mensagem_proposta: data.mensagem_proposta,
+        tipo_remetente: data.tipo_remetente,
+        updated: data.updated,
+        id_remetente: data.id_remetente,
+        id_destinatario: data.id_destinatario,
+        id_influenciador: data.id_influenciador,
+        data_envio: data.createdAt,
+        nome: data.Influenciador.nome,
+
+      }
+    })
+    return res.status(201).send(propstaComNome);
   }
-  
-  
-})
-const propstaComNome = propostas.map(data =>{
-  return{
-    id:data.id,
-    status_proposta:data.status_proposta ,
-    mensagem_proposta:data.mensagem_proposta ,
-		tipo_remetente:data.tipo_remetente ,
-		updated:data.updated ,
-		id_remetente:data.id_remetente ,
-		id_destinatario:data.id_destinatario ,
-		id_influenciador: data.id_influenciador,
-        data_envio:data.createdAt,
-    nome_influenciador:data.Influenciador.nome,
-    // nome_empresa : data.Empresa.nome
+
+  static async listPropostaFromEmpresaRecebidas(req, res) {
+    const idUser = req.params.id
+    const propostas = await Proposta.findAll({
+      where: {
+        id_empresa: idUser,
+        tipo_remetente: 'influenciador',
+      },
+      include: {
+        model: Influenciador,
+        required: true
+      },
+
+
+    })
+    const propstaComNome = propostas.map(data => {
+      return {
+        id: data.id,
+        status_proposta: data.status_proposta,
+        mensagem_proposta: data.mensagem_proposta,
+        tipo_remetente: data.tipo_remetente,
+        updated: data.updated,
+        id_remetente: data.id_remetente,
+        id_destinatario: data.id_destinatario,
+        id_influenciador: data.id_influenciador,
+        data_envio: data.createdAt,
+        nome: data.Influenciador.nome,
+
+      }
+    })
+    return res.status(201).send(propstaComNome);
   }
-})
-  return res.status(201).send(propstaComNome);
-}
-
-
-//---->Empresa Envio e Recebimento
-
-
-static async listPropostaFromEmpresaEnviadas(req, res){
-  const idUser = req.params.id
-  const propostas = await Proposta.findAll({
-  where:{
-    id_remetente: idUser,
-    tipo_remetente:'marca'
-  }})
-  return res.status(201).send(propostas);
-}
-
-static async listPropostaFromEmpresaRecebidas(req, res){
-  const idUser = req.params.id
-  const propostas = await Proposta.findAll({
-  where:{
-    empresaId :idUser,
-    tipo_remetente:'influenciador',
-  }})
-  return res.status(201).send(propostas);
-}
 
 
 
